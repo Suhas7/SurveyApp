9d10631e0ea90baf2a266a3dca9ebf3c48fc6497
Suhas <suhasraja1@gmail.com>
2020-12-15 15:12:24 -0600

    flawed visited logic

--- src/App.js
+++ src/App.js
@@ -9,18 +9,32 @@ function App() {
   console.log(data);
   const [catID, setCat] = useState(0);
   const [qID, setQID] = useState(0);
+  let visited = new Set();
   let reroll = ()=>{
     setCat(catID+1);
     setQID(qID+1);
   }
+  let szusdik = (a,b)=>{return a >= b ? a * a + a + b : a + b * b;}
+  let count = 1000;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>{data[catID]['title']}</h2>
-        <h4>{data[catID]['items'][qID]}</h4>
-        <Button onClick={()=>setCat((catID+1) % (Object.keys(data).map((el)=>parseInt(el)).reduce((a,b)=>Math.max(a,b))))} variant="contained">Category</Button>
-        <Button onClick={()=>setQID((qID+1) % data[catID]['items'].length)} variant="contained">Question</Button>
+        <h4>{data[catID]['items'][qID].replaceAll("*","")}</h4>
+        <Button onClick={()=> {
+          //while(visited.has(szusdik(catID,qID)) && count>0) {
+            setCat((catID + 1) % (Object.keys(data).map((el) => parseInt(el)).reduce((a, b) => Math.max(a, b))));
+          //  count--;
+          //}
+          visited.add(szusdik(catID,qID));
+        }} variant="contained">Next Category</Button>
+        <Button onClick={() => {
+          //while(visited.has(szusdik(catID,qID)) && count>0) {
+            setQID((qID + 1) % data[catID]['items'].length)
+          //}
+          //count--;
+        }} variant="contained">Next Question</Button>
       </header>
     </div>
   );
