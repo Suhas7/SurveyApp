191585fa914710573d4bd99dbf900b57cdb9068a
Suhas <suhasraja1@gmail.com>
2020-12-15 15:07:08 -0600

    basic functionality

--- qNoteToJSON.py
+++ qNoteToJSON.py
@@ -1,33 +1,34 @@
 import json
-questions = {"Uncategorized" : []}
 tokens = []
 z=""
 while(z!="fileEnd"):
     tokens.append(z)
     z=input()
-
 tokens=tokens[1:]
+headI=-1
 currHeader = "Uncategorized"
 currPrefix = ""
+questions = {-1 : {'title':'','items':[]}}
 for i in range(len(tokens)):
     if tokens[i].find(":")==-1 and tokens[i].find("?")==-1:
         currHeader=tokens[i]
-        questions[currHeader]=[]
+        headI+=1
+        questions[headI]={'title':currHeader,'items':[]}
         currPrefix=""
     elif tokens[i].find(":")!=-1:
         currPrefix=tokens[i]
     elif tokens[i].find("?")!=-1:
         if currPrefix!="":
-            questions[currHeader].append(currPrefix[:-1]+" "+tokens[i].lower())
+            questions[headI]['items'].append(currPrefix[:-1]+" "+tokens[i].lower())
         else:
-            questions[currHeader].append(tokens[i])
+            questions[headI]['items'].append(tokens[i])
 
 keys = questions.keys()
-questions = {k:v for k,v in questions.items() if len(v) !=0}
+questions = {k:v for k,v in questions.items() if len(v['items']) !=0}
 
 for k,v in questions.items():
-    print(k+":")
-    for el in v:
+    print(v['title']+":")
+    for el in v['items']:
         print("    "+el)
 
 with open('questions.json','w') as fp: json.dump(questions,fp)
--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import data from './questions.json'
+import {useState} from "react";
 
 function App() {
+  console.log(data);
+  const [catID, setCat] = useState(0);
+  const [qID, setQID] = useState(0);
+  let reroll = ()=>{
+    setCat(catID+1);
+    setQID(qID+1);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>{data[catID]['title']}</h2>
+        <h4>{data[catID]['items'][qID]}</h4>
+        <Button onClick={()=>setCat((catID+1) % (Object.keys(data).map((el)=>parseInt(el)).reduce((a,b)=>Math.max(a,b))))} variant="contained">Category</Button>
+        <Button onClick={()=>setQID((qID+1) % data[catID]['items'].length)} variant="contained">Question</Button>
       </header>
     </div>
   );
